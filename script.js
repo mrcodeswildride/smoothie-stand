07f81d6703170a3ff7a6d79a8455c7dcff1731f0
David Keller <mrcodeswildride@gmail.com>
2020-06-10 13:20:08 +0000

    es6 update

--- script.js
+++ script.js
@@ -1,266 +1,190 @@
-var strawberry = document.getElementById("strawberry");
-var blueberry = document.getElementById("blueberry");
-var cherry = document.getElementById("cherry");
-var blender = document.getElementById("blender");
-var smoothie = document.getElementById("smoothie");
-var moneyImage = document.getElementById("moneyImage");
+let strawberryImage = document.getElementById(`strawberryImage`)
+let blueberryImage = document.getElementById(`blueberryImage`)
+let cherryImage = document.getElementById(`cherryImage`)
+let blenderImage = document.getElementById(`blenderImage`)
+let smoothieImage = document.getElementById(`smoothieImage`)
+let moneyImage = document.getElementById(`moneyImage`)
 
-var hireStrawberryPickerButton = document.getElementById("hireStrawberryPicker");
-var hireBlueberryPickerButton = document.getElementById("hireBlueberryPicker");
-var hireCherryPickerButton = document.getElementById("hireCherryPicker");
-var hireOperatorButton = document.getElementById("hireOperator");
-var hireCashierButton = document.getElementById("hireCashier");
+let strawberriesParagraph = document.getElementById(`strawberriesParagraph`)
+let blueberriesParagraph = document.getElementById(`blueberriesParagraph`)
+let cherriesParagraph = document.getElementById(`cherriesParagraph`)
+let smoothiesParagraph = document.getElementById(`smoothiesParagraph`)
+let moneyParagraph = document.getElementById(`moneyParagraph`)
 
-var strawberriesDisplay = document.getElementById("strawberries");
-var blueberriesDisplay = document.getElementById("blueberries");
-var cherriesDisplay = document.getElementById("cherries");
-var smoothiesDisplay = document.getElementById("smoothies");
-var moneyDisplay = document.getElementById("money");
+let hireStrawberryPickerButton = document.getElementById(`hireStrawberryPickerButton`)
+let hireBlueberryPickerButton = document.getElementById(`hireBlueberryPickerButton`)
+let hireCherryPickerButton = document.getElementById(`hireCherryPickerButton`)
+let hireOperatorButton = document.getElementById(`hireOperatorButton`)
+let hireCashierButton = document.getElementById(`hireCashierButton`)
 
-var strawberryPickersDisplay = document.getElementById("strawberryPickers");
-var blueberryPickersDisplay = document.getElementById("blueberryPickers");
-var cherryPickersDisplay = document.getElementById("cherryPickers");
-var operatorsDisplay = document.getElementById("operators");
-var cashiersDisplay = document.getElementById("cashiers");
+let strawberryPickersParagraph = document.getElementById(`strawberryPickersParagraph`)
+let blueberryPickersParagraph = document.getElementById(`blueberryPickersParagraph`)
+let cherryPickersParagraph = document.getElementById(`cherryPickersParagraph`)
+let operatorsParagraph = document.getElementById(`operatorsParagraph`)
+let cashiersParagraph = document.getElementById(`cashiersParagraph`)
 
-var missedPaychecksDisplay = document.getElementById("missedPaychecks");
-var strikeMessage = document.getElementById("strikeMessage");
+let strawberries = 0
+let blueberries = 0
+let cherries = 0
+let smoothies = 0
+let money = 0
 
-var strawberries = 0;
-var blueberries = 0;
-var cherries = 0;
-var smoothies = 0;
-var money = 0;
+let strawberryPickers = 0
+let blueberryPickers = 0
+let cherryPickers = 0
+let operators = 0
+let cashiers = 0
 
-var strawberryPickers = 0;
-var blueberryPickers = 0;
-var cherryPickers = 0;
-var operators = 0;
-var cashiers = 0;
+strawberryImage.addEventListener(`click`, pickStrawberry)
+blueberryImage.addEventListener(`click`, pickBlueberry)
+cherryImage.addEventListener(`click`, pickCherry)
+blenderImage.addEventListener(`click`, blendFruits)
+smoothieImage.addEventListener(`click`, sellSmoothie)
 
-var missedPaychecks = 0;
-var strike = false;
-
-strawberry.addEventListener("click", pickStrawberry);
-blueberry.addEventListener("click", pickBlueberry);
-cherry.addEventListener("click", pickCherry);
-blender.addEventListener("click", blendFruits);
-smoothie.addEventListener("click", sellSmoothie);
-
-hireStrawberryPickerButton.addEventListener("click", hireStrawberryPicker);
-hireBlueberryPickerButton.addEventListener("click", hireBlueberryPicker);
-hireCherryPickerButton.addEventListener("click", hireCherryPicker);
-hireOperatorButton.addEventListener("click", hireOperator);
-hireCashierButton.addEventListener("click", hireCashier);
+hireStrawberryPickerButton.addEventListener(`click`, hireStrawberryPicker)
+hireBlueberryPickerButton.addEventListener(`click`, hireBlueberryPicker)
+hireCherryPickerButton.addEventListener(`click`, hireCherryPicker)
+hireOperatorButton.addEventListener(`click`, hireOperator)
+hireCashierButton.addEventListener(`click`, hireCashier)
 
 function pickStrawberry() {
-    strawberries = strawberries + 1;
-    updateFruits();
+  strawberries = strawberries + 1
+  updateFruits()
 }
 
 function pickBlueberry() {
-    blueberries = blueberries + 1;
-    updateFruits();
+  blueberries = blueberries + 1
+  updateFruits()
 }
 
 function pickCherry() {
-    cherries = cherries + 1;
-    updateFruits();
+  cherries = cherries + 1
+  updateFruits()
 }
 
 function blendFruits() {
-    if (strawberries >= 2 && blueberries >= 5 && cherries >= 3) {
-        strawberries = strawberries - 2;
-        blueberries = blueberries - 5;
-        cherries = cherries - 3;
-        updateFruits();
+  if (strawberries >= 2 && blueberries >= 5 && cherries >= 3) {
+    strawberries = strawberries - 2
+    blueberries = blueberries - 5
+    cherries = cherries - 3
+    updateFruits()
 
-        smoothies = smoothies + 1;
-        updateSmoothies();
-    }
+    smoothies = smoothies + 1
+    updateSmoothies()
+  }
 }
 
 function sellSmoothie() {
-    if (smoothies >= 1) {
-        smoothies = smoothies - 1;
-        updateSmoothies();
+  if (smoothies >= 1) {
+    smoothies = smoothies - 1
+    updateSmoothies()
 
-        money = money + 3;
-        updateMoney();
-    }
-}
-
-function hireStrawberryPicker() {
-    strawberryPickers = strawberryPickers + 1;
-    strawberryPickersDisplay.innerHTML = "Strawberry pickers: " + strawberryPickers;
-
-    setInterval(workerPickStrawberry, 1000);
-}
-
-function hireBlueberryPicker() {
-    blueberryPickers = blueberryPickers + 1;
-    blueberryPickersDisplay.innerHTML = "Blueberry pickers: " + blueberryPickers;
-
-    setInterval(workerPickBlueberry, 1000);
-}
-
-function hireCherryPicker() {
-    cherryPickers = cherryPickers + 1;
-    cherryPickersDisplay.innerHTML = "Cherry pickers: " + cherryPickers;
-
-    setInterval(workerPickCherry, 1000);
-}
-
-function hireOperator() {
-    operators = operators + 1;
-    operatorsDisplay.innerHTML = "Blender operators: " + operators;
-
-    setInterval(workerBlendFruits, 1000);
-}
-
-function hireCashier() {
-    cashiers = cashiers + 1;
-    cashiersDisplay.innerHTML = "Cashiers: " + cashiers;
-
-    setInterval(workerSellSmoothie, 1000);
-}
-
-function workerPickStrawberry() {
-    if (!strike) {
-        if (money >= 0.1) {
-            pickStrawberry();
-
-            money = money - 0.1;
-            updateMoney();
-        }
-        else {
-            missedPaychecks = missedPaychecks + 1;
-            updateMissedPaychecks();
-        }
-    }
-}
-
-function workerPickBlueberry() {
-    if (!strike) {
-        if (money >= 0.1) {
-            pickBlueberry();
-
-            money = money - 0.1;
-            updateMoney();
-        }
-        else {
-            missedPaychecks = missedPaychecks + 1;
-            updateMissedPaychecks();
-        }
-    }
-}
-
-function workerPickCherry() {
-    if (!strike) {
-        if (money >= 0.1) {
-            pickCherry();
-
-            money = money - 0.1;
-            updateMoney();
-        }
-        else {
-            missedPaychecks = missedPaychecks + 1;
-            updateMissedPaychecks();
-        }
-    }
-}
-
-function workerBlendFruits() {
-    if (!strike) {
-        if (money >= 0.9) {
-            blendFruits();
-
-            money = money - 0.9;
-            updateMoney();
-        }
-        else {
-            missedPaychecks = missedPaychecks + 1;
-            updateMissedPaychecks();
-        }
-    }
-}
-
-function workerSellSmoothie() {
-    if (!strike) {
-        if (money >= 0.9) {
-            sellSmoothie();
-
-            money = money - 0.9;
-            updateMoney();
-        }
-        else {
-            missedPaychecks = missedPaychecks + 1;
-            updateMissedPaychecks();
-        }
-    }
+    money = money + 3
+    updateMoney()
+  }
 }
 
 function updateFruits() {
-    strawberriesDisplay.innerHTML = "Strawberries: " + strawberries;
-    blueberriesDisplay.innerHTML = "Blueberries: " + blueberries;
-    cherriesDisplay.innerHTML = "Cherries: " + cherries;
+  strawberriesParagraph.innerHTML = `Strawberries: ${strawberries}`
+  blueberriesParagraph.innerHTML = `Blueberries: ${blueberries}`
+  cherriesParagraph.innerHTML = `Cherries: ${cherries}`
 
-    if (strawberries >= 2 && blueberries >= 5 && cherries >= 3) {
-        blender.style.opacity = "1";
-    }
-    else {
-        blender.style.opacity = "0.5";
-    }
+  if (strawberries >= 2 && blueberries >= 5 && cherries >= 3) {
+    blenderImage.style.opacity = `100%`
+  }
+  else {
+    blenderImage.style.opacity = `50%`
+  }
 }
 
 function updateSmoothies() {
-    smoothiesDisplay.innerHTML = "Smoothies: " + smoothies;
+  smoothiesParagraph.innerHTML = `Smoothies: ${smoothies}`
 
-    if (smoothies >= 1) {
-        smoothie.style.opacity = "1";
-    }
-    else {
-        smoothie.style.opacity = "0.5";
-    }
+  if (smoothies >= 1) {
+    smoothieImage.style.opacity = `100%`
+  }
+  else {
+    smoothieImage.style.opacity = `50%`
+  }
 }
 
 function updateMoney() {
-    moneyDisplay.innerHTML = "Money: $" + money.toFixed(2);
+  moneyParagraph.innerHTML = `Money: $${money.toFixed(2)}`
 
-    if (money >= 0.1) {
-        moneyImage.style.opacity = "1";
-    }
-    else {
-        moneyImage.style.opacity = "0.5";
-    }
-
-    if (money >= 0.1 & !strike) {
-        hireStrawberryPickerButton.disabled = false;
-        hireBlueberryPickerButton.disabled = false;
-        hireCherryPickerButton.disabled = false;
-        hireOperatorButton.disabled = false;
-        hireCashierButton.disabled = false;
-    }
-    else {
-        hireStrawberryPickerButton.disabled = true;
-        hireBlueberryPickerButton.disabled = true;
-        hireCherryPickerButton.disabled = true;
-        hireOperatorButton.disabled = true;
-        hireCashierButton.disabled = true;
-    }
+  if (money > 0) {
+    moneyImage.style.opacity = `100%`
+  }
+  else {
+    moneyImage.style.opacity = `50%`
+  }
 }
 
-function updateMissedPaychecks() {
-    missedPaychecksDisplay.innerHTML = "Missed paychecks: " + missedPaychecks;
+function hireStrawberryPicker() {
+  strawberryPickers = strawberryPickers + 1
+  strawberryPickersParagraph.innerHTML = `Strawberry pickers: ${strawberryPickers}`
 
-    if (missedPaychecks >= 50) {
-        strike = true;
-        strikeMessage.style.display = "initial";
+  setInterval(workerPickStrawberry, 1000)
+}
 
-        hireStrawberryPickerButton.disabled = true;
-        hireBlueberryPickerButton.disabled = true;
-        hireCherryPickerButton.disabled = true;
-        hireOperatorButton.disabled = true;
-        hireCashierButton.disabled = true;
-    }
+function hireBlueberryPicker() {
+  blueberryPickers = blueberryPickers + 1
+  blueberryPickersParagraph.innerHTML = `Blueberry pickers: ${blueberryPickers}`
+
+  setInterval(workerPickBlueberry, 1000)
+}
+
+function hireCherryPicker() {
+  cherryPickers = cherryPickers + 1
+  cherryPickersParagraph.innerHTML = `Cherry pickers: ${cherryPickers}`
+
+  setInterval(workerPickCherry, 1000)
+}
+
+function hireOperator() {
+  operators = operators + 1
+  operatorsParagraph.innerHTML = `Blender operators: ${operators}`
+
+  setInterval(workerBlendFruits, 1000)
+}
+
+function hireCashier() {
+  cashiers = cashiers + 1
+  cashiersParagraph.innerHTML = `Cashiers: ${cashiers}`
+
+  setInterval(workerSellSmoothie, 1000)
+}
+
+function workerPickStrawberry() {
+  pickStrawberry()
+
+  money = money - 0.1
+  updateMoney()
+}
+
+function workerPickBlueberry() {
+  pickBlueberry()
+
+  money = money - 0.1
+  updateMoney()
+}
+
+function workerPickCherry() {
+  pickCherry()
+
+  money = money - 0.1
+  updateMoney()
+}
+
+function workerBlendFruits() {
+  blendFruits()
+
+  money = money - 0.9
+  updateMoney()
+}
+
+function workerSellSmoothie() {
+  sellSmoothie()
+
+  money = money - 0.9
+  updateMoney()
 }
